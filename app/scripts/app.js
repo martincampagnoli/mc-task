4cef6d054da7a193a163bfc330b8c951a0f69fe6
Martín Campagnoli <martin.campagnoli@olx.com>
2017-06-28 13:54:34 +0200

    Add routing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,23 @@
  * Main module of the application.
  */
 angular
-  .module('mcTaskApp', []);
+  .module('mcTaskApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('');
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mcTaskApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mcTaskApp
+ */
+angular.module('mcTaskApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
