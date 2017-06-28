f1a499c7ec8dcf908fb5a7d55c5173d68f6804ec
Martín Campagnoli <martin.campagnoli@olx.com>
2017-06-28 13:48:07 +0200

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mcTaskApp
+ * @description
+ * # mcTaskApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mcTaskApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mcTaskApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mcTaskApp
+ */
+angular.module('mcTaskApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
