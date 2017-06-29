c2175e0cbcfb63316f6decadba7487c9abfcdeb9
Martín Campagnoli <martin.campagnoli@hotmail.com.ar>
2017-06-29 12:38:00 +0200

    Add custom view and live reload for scripts

--- app/scripts/controllers/main.controller.js
+++ app/scripts/controllers/main.controller.js
@@ -9,11 +9,13 @@
  */
 angular.module('mcTaskApp')
   .controller('MainCtrl', function ($scope, MainService) {
-
+    $scope.ads = [];
+    $scope.quantity = 10;
     $scope.getAds = function(){
     				  //UIHelper.blockUI();
     					MainService.getAds().then(function(response){
                   console.dir(response.data.data);
+                  $scope.ads = response.data.data;
     					       //UIHelper.unblockUI();
     						},
     						function(error){
